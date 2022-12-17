755a176b366cf74f4f709c5a65242a999329be6e
Sem Kirkels <101119408+SemKirkels@users.noreply.github.com>
2022-12-17 22:27:24 +0100

    Update namespace

--- Tic_Tac_Toe/cpu.cpp
+++ Tic_Tac_Toe/cpu.cpp
@@ -1,98 +1,101 @@
 #include "cpu.h"
 
-CPU::CPU(TicTacToeSemKirkels::Field *field) : field(field)
+namespace TicTacToeSemKirkels
 {
-
-}
-
-/*
- * Contains the logic for the cpu in a PvC game
- * On the first turn the cpu check who is the starting player.
- * If the cpu starts it plays a regular game
- * If the localplayer starts the cpu only tries to block it.
- */
-int CPU::placeSymbol()
-{
-    turn++;
-    checkField();
-
-    if(turn == 1)
+    CPU::CPU(TicTacToeSemKirkels::Field *field) : field(field)
     {
-        checkStartingPlayer();
 
-        if(grid[0] == ' ')
+    }
+
+    /*
+    * Contains the logic for the cpu in a PvC game
+    * On the first turn the cpu check who is the starting player.
+    * If the cpu starts it plays a regular game
+    * If the localplayer starts the cpu only tries to block it.
+    */
+    int CPU::placeSymbol()
+    {
+        turn++;
+        checkField();
+
+        if(turn == 1)
         {
-            return 1;
+            checkStartingPlayer();
+
+            if(grid[0] == ' ')
+            {
+                return 1;
+            }
+            else if(grid[2] == ' ')
+            {
+                return 3;
+            }
+            else
+            {
+                return (rand() % 9);
+            }
         }
-        else if(grid[2] == ' ')
+
+        if(turn == 2)
         {
-            return 3;
+            if(grid[0] == symbol)
+            {
+                return 9;
+            }
+            else if(grid[6] == ' ')
+            {
+                return 7;
+            }
+            else
+            {
+                return (rand() % 9);
+            }
         }
-        else
+
+        // Check for oponent win conditions
+
+        return (rand() % 9);
+    }
+
+    /*
+    * Copies the information from the field to a new array 'grid'
+    */
+    void CPU::checkField()
+    {
+        for(int i = 0; i < 9; i++)
         {
-            return (rand() % 9);
+            grid[i] = field->getSymbols(i);
         }
     }
 
-    if(turn == 2)
+    /*
+    * Check if the field is completely empty if so a flag 'player_start' is set to false
+    * Else the flag is set to true
+    */
+    void CPU::checkStartingPlayer()
     {
-        if(grid[0] == symbol)
+        for(int i = 0; i < 9; i++)
         {
-            return 9;
-        }
-        else if(grid[6] == ' ')
-        {
-            return 7;
-        }
-        else
-        {
-            return (rand() % 9);
+            int empty_boxes = 0;
+
+            if(grid[i] == ' ')
+            {
+                empty_boxes++;
+            }
+
+            if(empty_boxes == 8)
+            {
+                player_start = false;
+            }
+            else
+            {
+                player_start = true;
+            }
         }
     }
 
-    // Check for oponent win conditions
-
-    return (rand() % 9);
-}
-
-/*
- * Copies the information from the field to a new array 'grid'
- */
-void CPU::checkField()
-{   
-    for(int i = 0; i < 9; i++)
+    CPU::~CPU()
     {
-        grid[i] = field->getSymbols(i);
+
     }
-}
-
-/*
- * Check if the field is completely empty if so a flag 'player_start' is set to false
- * Else the flag is set to true
- */
-void CPU::checkStartingPlayer()
-{
-    for(int i = 0; i < 9; i++)
-    {
-        int empty_boxes = 0;
-
-        if(grid[i] == ' ')
-        {
-            empty_boxes++;
-        }
-
-        if(empty_boxes == 8)
-        {
-            player_start = false;
-        }
-        else
-        {
-            player_start = true;
-        }
-    }
-}
-
-CPU::~CPU()
-{
-
 }
--- Tic_Tac_Toe/cpu.h
+++ Tic_Tac_Toe/cpu.h
@@ -5,20 +5,23 @@
 #include "field.h"
 #include <cstdlib>
 
-class CPU : public TicTacToeSemKirkels::Player
+namespace TicTacToeSemKirkels
 {
-    public:
-    CPU(TicTacToeSemKirkels::Field *field);
-        int placeSymbol();
-        void checkField();
-        void checkStartingPlayer();
-        ~CPU();
-    protected:
-        char grid[9];
-        TicTacToeSemKirkels::Field *field;
-    private:
-        int turn = 0;
-        bool player_start = false;
-};
+    class CPU : public TicTacToeSemKirkels::Player
+    {
+        public:
+        CPU(TicTacToeSemKirkels::Field *field);
+            int placeSymbol();
+            void checkField();
+            void checkStartingPlayer();
+            ~CPU();
+        protected:
+            char grid[9];
+            TicTacToeSemKirkels::Field *field;
+        private:
+            int turn = 0;
+            bool player_start = false;
+    };
+}
 
 #endif // CPU_H
